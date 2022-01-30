3bd0fee6570d669a717970b29c536af71bf5e80b
Igor Vereshchagin <9868425@gmail.com>
2022-01-30 22:29:36 +0300

    fix: fix bugs with modifiers name

--- script/index.js
+++ script/index.js
@@ -3,8 +3,8 @@ const popupSection = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 
 const formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_name');
-let nicknameInput = formElement.querySelector('.popup__input_nickname');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let nicknameInput = formElement.querySelector('.popup__input_type_nickname');
 let profielName = document.querySelector('.profile__name');
 let profileNickname = document.querySelector('.profile__nickname');
 
