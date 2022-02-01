b1e0afb10917c79d200ec4b98288aef87ef68a49
Igor Vereshchagin <9868425@gmail.com>
2022-02-01 23:36:44 +0300

    feat: make rendering of the cards while loading

--- script/index.js
+++ script/index.js
@@ -1,3 +1,47 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const sectionElements = document.querySelector('.elements');
+const element = document.querySelector('.element');
+const template = document.querySelector('#template').content;
+
+function render(cards) {
+    cards.forEach(renderCards);
+}
+
+function renderCards(item) {
+    const newElement = template.cloneNode(true);
+    newElement.querySelector('.element__photo').src = item.link;
+    newElement.querySelector('.element__title').textContent = item.name;
+    sectionElements.append(newElement);
+}
+
+render(initialCards);
+
 const editProfileButton = document.querySelector('.profile__edit-button');
 const popupSection = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
@@ -29,3 +73,4 @@ function formSubmitHandler(event) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
