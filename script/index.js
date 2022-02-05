ce41849a018ffd3685b0d84b224d77c4ee2aadfd
Igor Vereshchagin <9868425@gmail.com>
2022-02-05 19:08:53 +0300

    feat: make functionality to add new cards

--- script/index.js
+++ script/index.js
@@ -25,10 +25,11 @@ const initialCards = [
     }
   ];
 
+// Выбираем секцию elements в которой отрисовывем изначальные карточки из массива, выбираем template для картинок, которые будет отрисовывать по контенту, содержащемуся в нем.
 const sectionElements = document.querySelector('.elements');
-const element = document.querySelector('.element');
 const template = document.querySelector('#template').content;
 
+// Создаем функцию для отрисовки карточек с вызовом функции по клонированию template с заполнением соответствующих полей и фото и заголовка, полученных из массива, вызываем функцию render
 function render(cards) {
     cards.forEach(renderCards);
 }
@@ -37,34 +38,41 @@ function renderCards(item) {
     const newElement = template.cloneNode(true);
     newElement.querySelector('.element__photo').src = item.link;
     newElement.querySelector('.element__title').textContent = item.name;
-    sectionElements.append(newElement);
+    sectionElements.prepend(newElement);
 }
 
 render(initialCards);
 
+// Создаем переменные для узлов кнопки редактирования профиля, блока попапа, в томч числе кнопки закрытия
 const editProfileButton = document.querySelector('.profile__edit-button');
 const popupSection = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 
+// Создаем переменные для формы попапа, input-ов внутри формы, в которые вводятся измененные данные профиля, а также полей профиля которые будут отрисованы нами при загрузке страницы по умолчанию
 const formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let nicknameInput = formElement.querySelector('.popup__input_type_nickname');
 let profielName = document.querySelector('.profile__name');
 let profileNickname = document.querySelector('.profile__nickname');
 
+// Создаем функцию для открытия окна popup, а также присваиваем полям попапа изначальные значения, полученные из полей профайла, по умолчанию при первой загрузке
 function openPopupWindow() {
     popupSection.classList.add('popup_opened');
     nameInput.value = profielName.textContent;
     nicknameInput.value = profileNickname.textContent;
 }
 
+// Прописываем функцию закрытия попапа при клике на крестик
 function closePopupWindow() {
     popupSection.classList.remove('popup_opened');
 }
 
+// Вешаем обработчики событий на кнопку редактирования профиля и на крестик закртия попапа
 editProfileButton.addEventListener('click', openPopupWindow);
 popupCloseButton.addEventListener('click', closePopupWindow);
 
+
+// Пишем функцию для вставки в поля профайла данных из интпутов попапа, введенные пользователем
 function formSubmitHandler(event) {
     event.preventDefault();
     profielName.textContent = nameInput.value;
@@ -72,17 +80,20 @@ function formSubmitHandler(event) {
     closePopupWindow();
 }
 
+// Вешаем обработчик событий на форму попапа, при нажатии кнопки сохранения
 formElement.addEventListener('submit', formSubmitHandler);
 
+
+// Сщздаем переменные в котрых храним поля нового попапа, кнопку профайла для добавления карточек
 const cardPopupSection = document.querySelector('.popup__cards');
 const addCard = document.querySelector('.profile__button');
 const popupCardCloseButton = document.querySelector('.popup__close_card');
 let cardInput = document.querySelector('.popup__input_card_title');
 let cardImage = document.querySelector('.popup__input_card_link');
+const formCardElement = document.querySelector('.popup__form_card');
 
 function openPopupCardWindow() {
   cardPopupSection.classList.add('popup_opened');
-    
 }
 
 function closePopupCardWindow() {
@@ -92,3 +103,15 @@ function closePopupCardWindow() {
 addCard.addEventListener('click', openPopupCardWindow);
 popupCardCloseButton.addEventListener('click', closePopupCardWindow);
 
+function cardSubmitHandler(event) {
+  event.preventDefault();
+  const newCard = 
+    {
+      name: cardInput.value,
+      link: cardImage.value
+    };
+  renderCards(newCard);
+  closePopupCardWindow();
+}
+
+formCardElement.addEventListener('submit', cardSubmitHandler);
